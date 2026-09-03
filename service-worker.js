5c829ce0d14bcbe6749183ef58733ff362b5e9fb
hollyfree85-ai <hollyfree85@gmail.com>
2026-09-03 18:05:42 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -67,8 +67,8 @@ self.addEventListener("notificationclick",event=>{
   })());
 });
 
-const CACHE="fz-tip-v1331";
-const CORE=["./","./index.html","./app-v1331.js","./simple-ui-v1330.js","./firebase-config.js","./push-config.js","./hourly-logic.js","./manifest.webmanifest", "./money-ready-chime.wav"];
+const CACHE="fz-tip-v1332";
+const CORE=["./","./index.html","./app-v1332.js","./simple-ui-v1332.js","./firebase-config.js","./push-config.js","./hourly-logic.js","./manifest.webmanifest", "./money-ready-chime.wav"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
