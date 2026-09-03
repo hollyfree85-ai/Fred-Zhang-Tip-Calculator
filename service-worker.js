6610d2b98165ba25dcab233f02361ae6db117afd
hollyfree85-ai <hollyfree85@gmail.com>
2026-09-03 02:14:43 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="fz-tip-v1172";
-const CORE=["./","./index.html","./app-v1172.js","./firebase-config.js","./hourly-logic.js","./manifest.webmanifest"];
+const CACHE="fz-tip-v1190";
+const CORE=["./","./index.html","./app-v1190.js","./firebase-config.js","./hourly-logic.js","./manifest.webmanifest"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
